78ff8f7d6f7b93bd164bf9920c5258e00a0342f5
Firuz Mammadov <firuznmmammadov@gmail.com>
2025-05-22 15:06:24 +0400

    #File Structure added

---
+++ src/SiteData/Data.jsx
@@ -0,0 +1,71 @@
+
+export const serviceData = [
+  {
+    id: 1,
+    title: "service-1",
+  },
+  {
+    id: 2,
+    title: "service-2",
+  },
+  {
+    id: 3,
+    title: "service-3",
+  },
+  {
+    id: 4,
+    title: "service-4",
+  },
+  {
+    id: 5,
+    title: "service-5",
+  },
+  {
+    id: 6,
+    title: "service-6",
+  },
+  {
+    id: 7,
+    title: "service-7",
+  },
+  {
+    id: 8,
+    title: "service-8",
+  },
+  {
+    id: 9,
+    title: "service-9",
+  },
+  {
+    id: 10,
+    title: "service-10",
+  },
+  {
+    id: 11,
+    title: "service-11",
+  },
+  {
+    id: 12,
+    title: "service-12",
+  },
+  {
+    id: 13,
+    title: "service-13",
+  },
+  {
+    id: 14,
+    title: "service-14",
+  },
+  {
+    id: 15,
+    title: "service-15",
+  },
+  {
+    id: 16,
+    title: "service-16",
+  },
+  {
+    id: 17,
+    title: "service-17",
+  },
+];
